81d4f2cf37ab7e64d9e7f8391459a28dbc0a8294
Gar-Yin Lam <garyin.lam@gmail.com>
2023-10-04 13:07:23 +0100

    explode request

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(function ($class){
+  require __DIR__ . "/src/$class.php";
+});
+
+header("Content-type: application/json; charset=UTF-8;");
+
+$parts = explode("/",$_SERVER["REQUEST_URI"]);
