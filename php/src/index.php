9921f71ee58b5f9f140fb4bc79900ba85a09de3d
Gar-Yin Lam <garyin.lam@gmail.com>
2023-10-04 17:18:29 +0100

    rearrage files for docker

--- php/src/index.php
+++ php/src/index.php
@@ -13,9 +13,14 @@ header("Content-type: application/json; charset=UTF-8;");
 
 $parts = explode("/",$_SERVER["REQUEST_URI"]);
 
+if(!str_starts_with($parts[1],'services')){
+  http_response_code(404);
+  exit;
+}
+
 $country = isset($_GET['country']) ? $_GET['country'] : null;
 
-$database = new Database("localhost", "instruct", "root", "password1");
+$database = new Database("db", "instruct", "root", "password1");
 
 $gateway = new ServicesGateway($database);
 
