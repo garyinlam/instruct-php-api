12b311e38d566adc69fdd0004142b71cf03d32b5
Gar-Yin Lam <garyin.lam@gmail.com>
2023-10-04 13:29:26 +0100

    Database file

--- index.php
+++ index.php
@@ -9,3 +9,6 @@ spl_autoload_register(function ($class){
 header("Content-type: application/json; charset=UTF-8;");
 
 $parts = explode("/",$_SERVER["REQUEST_URI"]);
+
+
+$database = new Database("localhost", "instruct", "root", "password1");
