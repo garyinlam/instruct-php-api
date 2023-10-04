871c86d72502dcb3233b4d3a724a87c010294cab
Gar-Yin Lam <garyin.lam@gmail.com>
2023-10-04 13:30:15 +0100

    ErrorHandler created and added

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ spl_autoload_register(function ($class){
   require __DIR__ . "/src/$class.php";
 });
 
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
+
 header("Content-type: application/json; charset=UTF-8;");
 
 $parts = explode("/",$_SERVER["REQUEST_URI"]);
